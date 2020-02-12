06101ba35e93f3f689b035c522718631ec3a7e26
t4rmo <t4rmogoyft@gmail.com>
2020-02-12 14:06:55 +0100

    Mejoras

--- generador_testcases/README.md
+++ generador_testcases/README.md
@@ -6,10 +6,5 @@ Ejemplo:
 
 
 
-➜  generador_testcases git:(master) ✗ python generador_testcases.py -g 4 2 1 3 1
-
-
-
-
 
 
--- generador_testcases/generador_testcases.py
+++ generador_testcases/generador_testcases.py
@@ -9,17 +9,24 @@ def randomword(length):
 
 class Generator_test():
 
-    def imprimir_matriz(self,rows=1,columns=1,char=" ",n=1,zero=False):
+    def imprimir_ristra(self,rows=1,columns=1,char=" ",ncar=1,n=1,word=False,zero=False):
         a=""
         for k in range(n):
+            print(n)
             for i in range(rows):
                 for j in range(columns):
-                    if zero==False:
-                        a+=str(random.randint(0,9))
-                        if j != columns-1:
-                            a+=char
+                    if word == False:
+                        if zero==False:
+                            for l in range(ncar):
+                                a+=str(random.randint(0,9))
+                            if j != columns-1:
+                                a+=char
+                        else:
+                            a+=str(0)
+                            if j != columns-1:
+                                a+=char
                     else:
-                        a+=str(0)
+                        a+=randomword(ncar)
                         if j != columns-1:
                             a+=char
 
@@ -28,22 +35,6 @@ class Generator_test():
             print(a)
             a=""
 
-    def imprimir_ristra(self,rows,ncar,nveces=1,word=False,zero=False):
-        text=""
-        for k in range(nveces):
-            if nveces>1:
-                print(str(rows))
-            for i in range(rows):
-                if word!=False:
-                    text+=randomword(ncar)
-                else:
-                    if zero==False:
-                        for j in range(ncar):
-                            text+=str(random.randint(0,9))
-                    else:
-                        text+=str(0)*ncar
-                print(text)
-                text=""
 
 #    def imprimir_matriz(self,fmt):
 #        if fmt == 'std':
@@ -59,14 +50,13 @@ gen=Generator_test()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', dest='format', choices=['std', 'iso', 'unix', 'tz'],
-                    help="shows datetime in given format")
 parser.add_argument('-M', type=str,nargs='*',dest='matrix',help="Genera matriz nxn")
 
-parser.add_argument('-g', type=int,nargs='*',dest='gen',help="Genera texto")
+#parser.add_argument('-g', type=int,nargs='*',dest='gen',help="Genera texto")
+#parser.add_argument('-f', dest='format', choices=['std', 'iso', 'unix', 'tz'],help="shows datetime in given format")
 
 args = parser.parse_args()
-fmt = args.format
+#fmt = args.format
 
 #Cutre
 if args.matrix:
@@ -74,12 +64,7 @@ if args.matrix:
     a2 = int(args.matrix[1])
     a3 = str(args.matrix[2])
     a4 = int(args.matrix[3])
-    gen.imprimir_matriz(a1,a2,a3,a4)
-elif args.gen:
-    a1 = int(args.gen[0])
-    a2 = int(args.gen[1])
-    a3 = int(args.gen[2])
-    a4 = int(args.gen[3])
-    a5 = int(args.gen[4])
-    gen.imprimir_ristra(a1,a2,a3,a4,a5)
+    a5 = int(args.matrix[4])
+    a6 = int(args.matrix[5])
+    gen.imprimir_ristra(a1,a2,a3,a4,a5,a6)
 
