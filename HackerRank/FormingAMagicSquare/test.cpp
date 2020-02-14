93c2f8ceebf02857a66c0278b3809f6c9b465e48
augur97 <gonza897@hotmail.com>
2020-02-14 19:10:16 +0100

    Add formating for c++ and python

--- HackerRank/FormingAMagicSquare/test.cpp
+++ HackerRank/FormingAMagicSquare/test.cpp
@@ -4,43 +4,37 @@ using namespace std;
 
 // Complete the formingMagicSquare function below.
 int formingMagicSquare(vector<vector<int>> s) {
-    vector<vector<vector<int>>> mat {
-        { 8, 1, 6 },
-        { 3, 5, 7 },
-        { 4, 9, 2 } };
-                                    
-    for(int j=0;j<3;j++)
-    {
-        for(int k=0;k<8;k++)
-        {
-            cost[k] += abs(s[i][j] - mat[k][i][j]);
-        }
+  vector<vector<vector<int>>> mat{{8, 1, 6}, {3, 5, 7}, {4, 9, 2}};
+
+  for (int j = 0; j < 3; j++) {
+    for (int k = 0; k < 8; k++) {
+      cost[k] += abs(s[i][j] - mat[k][i][j]);
     }
-    
-    sort(cost.begin(),cost.end());
-    return cost[0];
+  }
+
+  sort(cost.begin(), cost.end());
+  return cost[0];
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+int main() {
+  ofstream fout(getenv("OUTPUT_PATH"));
 
-    vector<vector<int>> s(3);
-    for (int i = 0; i < 3; i++) {
-        s[i].resize(3);
+  vector<vector<int>> s(3);
+  for (int i = 0; i < 3; i++) {
+    s[i].resize(3);
 
-        for (int j = 0; j < 3; j++) {
-            cin >> s[i][j];
-        }
-
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for (int j = 0; j < 3; j++) {
+      cin >> s[i][j];
     }
 
-    int result = formingMagicSquare(s);
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  }
 
-    fout << result << "\n";
+  int result = formingMagicSquare(s);
 
-    fout.close();
+  fout << result << "\n";
 
-    return 0;
+  fout.close();
+
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # CuartaMatricula
-Repositorio para almacenar código de programación competitiva. 
+Repositorio para almacenar código de programación competitiva.
 Leer la forma que tenemos de organizar el código para no machacar nada y no liarnos.
+
+El código se formatea con [black](https://github.com/psf/) para python
+```bash
+python3 -m pip install black --user
+```
+y [clang-format](https://clang.llvm.org/docs/ClangFormat.html) para c++
+```bash
+sudo apt install clang-format # en ubuntu
+sudo pacman -S clang # en arch
+http://releases.llvm.org/download.html # windows
+```
 Licencia MIT.
 ## Integrantes del aquipo:
 Alvaro Iglesias.
