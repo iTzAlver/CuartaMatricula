4fea7c74eec11e819e2b71a90801bfcd5799eb44
t4rmo <t4rmogoyft@gmail.com>
2020-02-19 16:32:33 +0100

    Ligeras modificaciones

--- utils/generador_testcases/generador_testcases.py
+++ utils/generador_testcases/generador_testcases.py
@@ -36,7 +36,7 @@ class Utiles:
 
 class Generator_test:
     def imp_ristra(
-        self, rows, columns, char=" ", ncar=1, tipo=0, iniciot="0" , finalt="0" , fijado="" , inicionf="0" , finalnf="0" , nfijado="0" , n=1
+        self, rows, columns, char=" ", tipo=0, ncar=1, iniciot="0" , finalt="0" , fijado="" , inicionf="0" , finalnf="0" , nfijado="0" , n=1
     ):
         a = ""
         b=0
@@ -63,7 +63,6 @@ class Generator_test:
                                 
                             ind1+=1
                             
-                    
                     b+=(1+len(char))
                     contc += 1
 
@@ -79,30 +78,31 @@ class Generator_test:
             a = ""
             b=0
 
-imprimir_ronda=True
 
 gen = Generator_test()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-M", type=str, nargs=5, dest="matrix", help="Genera matriz nxn")
+parser.add_argument("-M", type=str, nargs=4, dest="matrix", help="Genera matriz nxn")
 
-parser.add_argument('-i', type=str, nargs="*", dest="intercalado", help='intercalado')
+parser.add_argument("-i", type=str, nargs="*", dest="intercalado", help='Elige que posiciones son caracteres o numeros')
 
-parser.add_argument('-f', type=str, nargs="*", dest="fijado", help='fijado')
+parser.add_argument("-f", type=str, nargs="*", dest="fijado", help='fijado de caracteres en algunos caracteres')
 
-parser.add_argument('-n', type=str, nargs=1, dest="size", help='n lineas')
+parser.add_argument("--np", help="Elimina nº ronda",action="store_true")
 
+parser.add_argument("-nlineas", type=str, nargs=1, dest="size", help='numero de lineas')
+
+parser.add_argument("-nchar", type=int, nargs=1, dest="nchar", help='numero de caracteres por columna')
 
 args = parser.parse_args()
-
 
 if args.matrix:
     a1 = str(args.matrix[0])
     a2 = str(args.matrix[1])
     a3 = str(args.matrix[2])
     a4 = int(args.matrix[3])
-    a5 = int(args.matrix[4])
+    a5 = 1
     a6="0"
     a7="0"
     a8=""
@@ -110,6 +110,9 @@ if args.matrix:
     a10=""
     a11=""
     a12=[1]
+
+if args.nchar:
+    a5 = int(args.nchar[0])
 
 if args.intercalado: 
     a6 = str(args.intercalado[0])
@@ -129,19 +132,18 @@ if args.fijado:
         a9 = str(args.fijado[1])
         a10 = str(args.fijado[2])
         a11 = str(args.fijado[3]) 
+
 if args.size:
     a12 = str(args.size[0]) 
     a12=Utiles.check_argument_list(a12)
-#if args.nprint:
-    #imprimir_ronda=False
-    
+
 
 cuentar=0
 cuentac=0
 a1=Utiles.check_argument_list(a1)
 a2=Utiles.check_argument_list(a2)
 for i in a12:
-    if imprimir_ronda == True:
+    if args.np == False:
         print(i)
     gen.imp_ristra(a1[cuentar], a2[cuentac], a3, a4, a5, a6, a7 , a8 , a9 , a10 , a11 , int(i))
     if(cuentar<len(a1)-1):
