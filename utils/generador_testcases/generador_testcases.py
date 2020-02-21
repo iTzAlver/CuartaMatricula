6ba64a0114925bee03598a9856f06b0c6df68263
t4rmo <t4rmogoyft@gmail.com>
2020-02-21 11:48:40 +0100

    Reorganizacion

--- utils/generador_testcases/generador_testcases.py
+++ utils/generador_testcases/generador_testcases.py
@@ -2,13 +2,11 @@ import argparse
 import random
 import string
 
-
 class Utiles:
     @staticmethod
     def randomword(length):
         letters = string.ascii_lowercase
         return "".join(random.choice(letters) for i in range(length))
-
     @staticmethod
     def randomnumber(length, i1, i2):
         return "".join(str(random.randint(i1, i2)) for i in range(length))
@@ -29,22 +27,16 @@ class Utiles:
                     a=int(arg)
                 except ValueError:
                     a=0
-
                 aux.append(a)
-    
         return aux
 
 class Generator_test:
-    def imp_ristra(
-        self, rows, columns, char=" ", tipo=0, ncar=1, iniciot="0" , finalt="0" , fijado="" , inicionf="0" , finalnf="0" , nfijado="0" , n=1
-    ):
+    def imp_ristra(self, rows, columns, char=" ", tipo=0, ncar=1, iniciot="0" , finalt="0" , fijado="" , inicionf="0" , finalnf="0" , nfijado="0" , n=1):
         a = ""
         b=0
         ind1=0
-
         contc = 1
         contf = 1
-
         for k in range(n):
             for i in range(rows):
                 for j in range(columns):
@@ -52,49 +44,36 @@ class Generator_test:
                         a += Utiles.randomnumber(ncar, 0, 9)
                     else:
                         a += Utiles.randomword(ncar)
- 
                     if j != columns - 1:
                         a += char
-                     
                     if len(fijado)>0 and ind1<len(fijado): 
                         if (contc >= int(inicionf) and contc <=int(finalnf)) and (str(contf) not in nfijado):
                             if fijado != "" and fijado[ind1] != "^" :
                                 a=a[:b]+fijado[ind1]+a[(b+1):]
-                                
                             ind1+=1
-                            
                     b+=(1+len(char))
                     contc += 1
-
                 if i != rows - 1:
                     a += "\n"
                     b = len(a)
-                    
                 contc=1
                 ind1 = 0
                 contf+=1
-
             print(a)
             a = ""
             b=0
-
 
 gen = Generator_test()
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-M", type=str, nargs=4, dest="matrix", help="Genera matriz nxn")
-
-parser.add_argument("-i", type=str, nargs="*", dest="intercalado", help='Elige que posiciones son caracteres o numeros')
-
-parser.add_argument("-f", type=str, nargs="*", dest="fijado", help='fijado de caracteres en algunos caracteres')
-
-parser.add_argument("--notnlineas", help="Elimina nº ronda",action="store_true")
-
-parser.add_argument("--notncol", help="Elimina nº columnas",action="store_true")
-
-parser.add_argument("-nlineas", type=str, nargs=1, dest="size", help='numero de lineas')
-
+parser.add_argument("-int", type=str, nargs="*", dest="intercalado", help='Elige que posiciones son caracteres o numeros')
+parser.add_argument("-fij", type=str, nargs="*", dest="fijado", help='fijado de caracteres en algunos caracteres')
+parser.add_argument("--nonl", help="No imprime el nº ronda",action="store_true")
+parser.add_argument("--nonc", help="No imprime el nº columnas",action="store_true")
+parser.add_argument("--invn", help="Invierte el orden de los mensajes nº ronda nº columna",action="store_true")
+parser.add_argument("-nlin", type=str, nargs=1, dest="size", help='numero de lineas')
 parser.add_argument("-nchar", type=int, nargs=1, dest="nchar", help='numero de caracteres por columna')
 
 args = parser.parse_args()
@@ -139,15 +118,19 @@ if args.size:
     a12 = str(args.size[0]) 
     a12=Utiles.check_argument_list(a12)
 
-
 cuentar=0
 cuentac=0
+inv=False
 a1=Utiles.check_argument_list(a1)
 a2=Utiles.check_argument_list(a2)
 for i in a12:
-    if args.notnlineas == False:
+    if args.invn == True:
+        print(a2[cuentar])
         print(i)
-    if args.notncol == False:
+        inv=True
+    if args.nonl == False and not inv:
+        print(i)
+    if args.nonc == False and not inv:
         print(a2[cuentar])
     gen.imp_ristra(a1[cuentar], a2[cuentac], a3, a4, a5, a6, a7 , a8 , a9 , a10 , a11 , int(i))
     if(cuentar<len(a1)-1):
